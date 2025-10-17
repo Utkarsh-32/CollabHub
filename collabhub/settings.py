237de782ccac3b28c0ecaeebdcfe00f829325906
Utkarsh-32 <utkarsh.shendre05@gmail.com>
2025-10-17 17:47:36 +0530

    chore(config): frontend URL from environment

--- collabhub/settings.py
+++ collabhub/settings.py
@@ -19,6 +19,8 @@ AUTH_USER_MODEL = "users.User"
 
 env = environ.Env(DEBUG=(bool, False))
 
+FRONTEND_URL = env('FRONTEND_URL')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
